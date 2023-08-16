0a272984e72423d687f7e0d599dbb05692a0fdcd
Kenny <kenny-maeder@web.de>
2023-08-16 13:37:07 +0200

    decided to make onepager

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,12 @@ export default function Home() {
   return (
     <main className="min-h-screen flex-col items-center justify-between">
       <Navibar/>
+      <div id="hsvhome">
+        <p>HOME</p>
+      </div>
+      <div id="uVerein">
+        <p>UNSER VEREIN</p>
+      </div>
     </main>
   )
 }
