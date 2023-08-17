268fd0e729b2ed5092989a53217b587afaa69c71
Kenny <kenny-maeder@web.de>
2023-08-17 09:03:30 +0200

    first texts

--- src/app/page.js
+++ src/app/page.js
@@ -4,13 +4,15 @@ export default function Home() {
   return (
     <main className="min-h-screen flex-col items-center justify-between">
       <Navibar/>
-      <div id="hsvhome">
-			<p><strong>Willkommen beim Hundesportverein Leipzig-Süd/Markkleeberg e.V.!</strong></p>
-<p>Wir bieten Ihnen und Ihrem Vierbeiner Abwechslung und Beschäftigung für den Hundealltag und verstehen uns als Verein für Rassehunde und Mischlinge aller Größen.<br />
-Unsere ehrenamtlichen, engagierten Trainer stehen Ihnen gern mit Ihrer Erfahrung als kompetente Ansprechpartner für alle Fragen der Hundeausbildung zur Seite.</p>
-<p>Unser breit gefächertes Angebot erstreckt sich über<br />
-Welpenstunde und Junghundegruppe, Gehorsamstraining, Begleithundeausbildung, Agility und Turnierhundesport, Fährtenarbeit und aktive Jugendförderung sowie Präsentationen auf öffentlichen Veranstaltungen.</p>
-<p>Wir freuen uns auf Ihren Besuch und bieten Ihnen unverbindlich ein <strong>kostenloses Schnuppertraining</strong> an!</p>
+      <div className="flex justify-center bg-[url('./../../pictures/dog_home.jpg')] bg-no-repeat bg-center bg-cover pt-10" id="hsvhome">
+        <div>
+          <h1 className="flex justify-center pt-10 pb-10 font-bold">Willkommen beim Hundesportverein Leipzig-Süd/Markkleeberg e.V.!</h1>
+          <p className="flex justify-center">Wir bieten Ihnen und Ihrem Vierbeiner Abwechslung und Beschäftigung für den Hundealltag und verstehen uns als Verein für Rassehunde und Mischlinge aller Größen.</p>
+          <p className="flex justify-center">Unsere ehrenamtlichen, engagierten Trainer stehen Ihnen gern mit Ihrer Erfahrung als kompetente Ansprechpartner für alle Fragen der Hundeausbildung zur Seite.</p>
+          <p className="flex justify-center">Unser breit gefächertes Angebot erstreckt sich über</p>
+          <p className="flex justify-center">Welpenstunde und Junghundegruppe, Gehorsamstraining, Begleithundeausbildung, Agility und Turnierhundesport, Fährtenarbeit und aktive Jugendförderung sowie Präsentationen auf öffentlichen Veranstaltungen.</p>
+          <p className="flex justify-center">Wir freuen uns auf Ihren Besuch und bieten Ihnen unverbindlich ein kostenloses Schnuppertraining an!</p>
+        </div>
       </div>
       <div id="uVerein">
         <p>UNSER VEREIN</p>
